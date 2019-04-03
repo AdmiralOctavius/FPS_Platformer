f808203d54499c56267ea17838fb350c8ebf2814
AdmiralOctavius <buildgamesnotbombs@gmail.com>
2019-04-03 18:37:48 -0400

    Fixed rotation, started gun1

--- Assets/PlayerFPSController_v1.cs
+++ Assets/PlayerFPSController_v1.cs
@@ -18,6 +18,11 @@ public class PlayerFPSController_v1 : MonoBehaviour {
     //Ver2 variables
     public float currentview;
 
+    //Ver3 Variables
+    public RaycastHit fireCast;
+    public float explosionForceVar;
+    public float explosionRadius;
+
     // Use this for initialization
     void Start () {
 
@@ -51,9 +56,39 @@ public class PlayerFPSController_v1 : MonoBehaviour {
         transform.Rotate(0, mouseXaxis, 0);
         //playerCamera.GetComponent<Transform>().transform.Rotate(mouseYaxis, 0, 0);
         //playerCamera.GetComponent<Transform>().transform.localEulerAngles.y 
+
         currentview = Mathf.Clamp(mouseYaxis, -60, 60);
-        playerCamera.GetComponent<Transform>().transform.localEulerAngles.x = Mathf.Clamp(mouseYaxis, -60, 60);
+        //currentview = mouseYaxis;
+
+        //playerCamera.GetComponent<Transform>().transform.localEulerAngles = new Vector3(currentview, playerCamera.GetComponent<Transform>().transform.localEulerAngles.y);
         playerCamera.GetComponent<Transform>().transform.Rotate(currentview, 0, 0);
+
+        if(playerCamera.GetComponent<Transform>().transform.localEulerAngles.x >= 60f && playerCamera.GetComponent<Transform>().transform.localEulerAngles.x <= 300f)
+        {
+            if (playerCamera.GetComponent<Transform>().transform.localEulerAngles.x > 60f)
+            {
+                if (playerCamera.GetComponent<Transform>().transform.localEulerAngles.x > 300f)
+                {
+                    //Debug.Log("Do nothing");
+                }
+                else if (playerCamera.GetComponent<Transform>().transform.localEulerAngles.x > 60f && playerCamera.GetComponent<Transform>().transform.localEulerAngles.x < 260f)
+                {
+                    //Debug.Log("Got here with x value of: " + playerCamera.GetComponent<Transform>().transform.localEulerAngles.x);
+                    playerCamera.GetComponent<Transform>().transform.localEulerAngles = new Vector3(60f, playerCamera.GetComponent<Transform>().transform.localEulerAngles.y);
+                }
+                else if (playerCamera.GetComponent<Transform>().transform.localEulerAngles.x < 300f && playerCamera.GetComponent<Transform>().transform.localEulerAngles.x > 260f)
+                {
+                  //  Debug.Log("Got here with x value of: " + playerCamera.GetComponent<Transform>().transform.localEulerAngles.x);
+                    playerCamera.GetComponent<Transform>().transform.localEulerAngles = new Vector3(300f, playerCamera.GetComponent<Transform>().transform.localEulerAngles.y);
+                }
+            }
+            else
+            {
+                //Debug.Log("Got here with x value of: " + playerCamera.GetComponent<Transform>().transform.localEulerAngles.x);
+                playerCamera.GetComponent<Transform>().transform.localEulerAngles = new Vector3(-60f, playerCamera.GetComponent<Transform>().transform.localEulerAngles.y);
+            }
+
+        }
 
         if (Input.GetAxis("Vertical") != 0)
         {
@@ -71,13 +106,13 @@ public class PlayerFPSController_v1 : MonoBehaviour {
         if(Input.GetAxis("Horizontal") != 0)
         {
             Debug.Log("Moving left or Right");
-            this.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(horizontalSpeed, 0, 0));
+            //this.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(horizontalSpeed, 0, 0));
 
             if (Input.GetAxis("Horizontal") > 0)
             {
                 this.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(horizontalSpeed, 0, 0));
             }
-            else
+            else    
             {
                 this.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(-horizontalSpeed, 0, 0));
             }
@@ -87,9 +122,17 @@ public class PlayerFPSController_v1 : MonoBehaviour {
         {
             Debug.Log("Firing");
             //firePoint.GetComponent<Transform>()
-            if(Physics.Raycast(firePoint.GetComponent<Transform>().localPosition,firePoint.GetComponent<Transform>().forward, 5f))
+            if(Physics.Raycast(firePoint.GetComponent<Transform>().localPosition,firePoint.GetComponent<Transform>().forward, out fireCast, 10f))
             {
                 Debug.Log("Got spot on wall");
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * fireCast.distance, Color.yellow);
+                Debug.Log(fireCast.distance);
+                if (fireCast.distance <= 7f)
+                {
+                    Debug.Log("In Distance, commencing movement");
+                    this.GetComponent<Rigidbody>().AddExplosionForce(explosionForceVar, fireCast.point, explosionRadius);
+                }
+                
             }
             
         }
